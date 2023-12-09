6faa1b3144b3d5e3fa598a276516c248b0f12c0f
Ily3s <ilyes.bennanii@gmail.com>
2023-12-09 23:10:14 +0100

    day9

--- src/day09.c
+++ src/day09.c
@@ -16,8 +16,8 @@ void bad_input()
     exit(-1);
 }
 
-#define MAX_LINE_NUMBER 801
-#define MAX_LINE_SIZE 400
+#define MAX_LINE_NUMBER 201
+#define MAX_LINE_SIZE 200
 
 bool eol(char c) {return !c || c == '\n' || c == '\r';}
 
@@ -35,4 +35,36 @@ int main(int argc, char**argv)
     // For the sake of speed, what follows suppose the input is valid
     clock_t start_point = clock();
 
-    uint64_t output1 = 0, output2 = 0;
+    int64_t output1 = 0, output2 = 0;
+
+    int64_t vals[50];
+
+    for (int i = 0; i < line_nb; i++)
+    {
+        int val_nb = 0;
+        for (char* ptr = input[i]; !eol(*ptr); ptr++) {
+            vals[val_nb] = strtoll(ptr, &ptr, 10);
+            val_nb++;
+        }
+        bool fun_is_null = false;
+        for (int k = 0; !fun_is_null; k++)
+        {
+            fun_is_null = true;
+            output1 += vals[val_nb-1];
+            output2 += vals[0] * (1-2*(k%2));
+            for (int j = 0; j < val_nb - 1; j++) {
+                vals[j] = vals[j+1] - vals[j];
+                fun_is_null &= !vals[j];
+            }
+            val_nb--;
+        }
+    }
+
+    clock_t end_point = clock();
+
+    printf("PART 1 output : %"PRIi64"\n", output1);
+    printf("PART 2 output : %"PRIi64"\n", output2);
+    printf("Time : %lf micro-seconds\n", (double)(end_point - start_point)/CLOCKS_PER_SEC*1000000.0);
+
+    return 0;
+}
